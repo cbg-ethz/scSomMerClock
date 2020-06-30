c2695979f7b8bef8eb3ece4f458fac562b5072ea
Nico Borgsmueller <nico.borgsmueller@bsse.ethz.ch>
2020-06-30 11:57:50 +0200

    profile for hpcs running with lsf added

---
+++ hpc/lsf/CookieCutter.py
@@ -0,0 +1,24 @@
+class CookieCutter:
+    """
+    Cookie Cutter wrapper
+    """
+
+    @staticmethod
+    def get_default_threads() -> int:
+        return int("1")
+
+    @staticmethod
+    def get_default_mem_mb() -> int:
+        return int("4096")
+
+    @staticmethod
+    def get_log_dir() -> str:
+        return "logs/cluster"
+
+    @staticmethod
+    def get_default_queue() -> str:
+        return ""
+
+    @staticmethod
+    def get_lsf_unit_for_limits() -> str:
+        return "MB"
