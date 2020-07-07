dbfa20b2c8828fb7f4dc27fd3f1ba6d5cc5a5193
NBMueller <nicoborgsmueller@web.de>
2020-07-07 09:43:53 +0200

    bugfixes

--- scripts/2.1_bwa.sh
+++ scripts/2.1_bwa.sh
@@ -2,6 +2,7 @@
 
 module purge
 
+pair_end=true
 while [ "$1" != "" ]; do
     key=$1
     case ${key} in
@@ -16,6 +17,9 @@ while [ "$1" != "" ]; do
                             ;;
         -l | --lib )        shift
                             WGA_LIBRARY=$1
+                            ;;
+        -se | --single-end )
+                            pair_end=false
                             ;;
     esac
     shift
@@ -35,11 +39,24 @@ RG="@RG\\tID:${sample}\\tSM:${SM}\\tPL:${PL}\\tLB:${LB}\\tPU:${PU}"
 
 if [[ ${WGA_LIBRARY} == "AMPLI-1" ]] || [[ ${WGA_LIBRARY} == "MALBAC" ]] || [[ ${WGA_LIBRARY} == "PICOPLEX" ]]
 then    
-    bwa mem -t 10 -R ${RG} ${REF} \
-        Processing/${sample}.trimmed2_1.fastq.gz Processing/${sample}.trimmed2_2.fastq.gz \
-        > Processing/${sample}.sam
+    if [ "${pair_end}" = true ]
+    then
+        bwa mem -t 10 -R ${RG} ${REF} \
+            Processing/${sample}.trimmed2_1.fastq.gz Processing/${sample}.trimmed2_2.fastq.gz \
+            > Processing/${sample}.sam
+    else
+        bwa mem -t 10 -R ${RG} $REF Processing/${sample}.trimmed2_1.fastq.gz \
+            > Processing/${sample}.sam
+
+    fi
 else
-    bwa mem -t 10 -R ${RG} ${REF} \
-        Processing/${sample}.trimmed_1.fastq.gz Processing/${sample}.trimmed_2.fastq.gz \
-        > Processing/${sample}.sam
+    if [ "${pair_end}" = true ]
+    then
+        bwa mem -t 10 -R ${RG} ${REF} \
+            Processing/${sample}.trimmed_1.fastq.gz Processing/${sample}.trimmed_2.fastq.gz \
+            > Processing/${sample}.sam
+    else
+        bwa mem -t 10 -R ${RG} ${REF} Processing/${sample}.trimmed_1.fastq.gz \
+            > Processing/${sample}.sam
+    fi
 fi
