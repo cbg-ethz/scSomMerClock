cda51625e3580b74499ac59fb28f1c092706e986
NBMueller <nicoborgsmueller@web.de>
2020-07-07 11:03:25 +0200

    snake output logged to file

--- wrapper.sh
+++ wrapper.sh
@@ -24,5 +24,5 @@ echo 'Running Snakemake:'
 echo "${SNAKE_CMD}"
 echo "Logging output to: ${LOGFILE}"
 
-module purge
-module load snakemake && ${SNAKE_CMD} &> ${LOGFILE}
+module purge > /dev/null
+module load snakemake > /dev/null && ${SNAKE_CMD} &> ${LOGFILE}
