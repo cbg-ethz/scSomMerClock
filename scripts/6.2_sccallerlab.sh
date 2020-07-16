7a85ac72c6cf9328748035867d98c9a52a13eb74
NBMueller <nicoborgsmueller@web.de>
2020-07-16 14:47:07 +0200

    bugfix

--- scripts/6.2_sccallerlab.sh
+++ scripts/6.2_sccallerlab.sh
@@ -19,16 +19,24 @@ done
 
 [[ -z "$out_file" ]] && { echo "Error: Output file not set"; exit 1; }
 
-# && bgzip --stdout --index Calls/${cellname}.real.${chr}.sccaller.vcf \
-    # > Calls/${cellname}.real.${chr}.sccaller.vcf.gz \
+for sample in ${sample_bams}
+do
+    bcftools view \
+        --regions 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,X,Y \
+        --output ${out_file}.gz \
+        --output-type z
+done
 
 cores=$(nproc)
 
-sorted_bams=$(echo "${sample_bams}" | sort -V --field-separator=. --key=3 | tr '\n' ' ') # | sed 's/$/.gz/'
+sorted_bams=$(echo "${sample_bams}" \
+    | sort -V --field-separator=. --key=3 \
+    | tr '\n' ' ' \
+    | sed 's/$/.gz/' 
+)
 echo $sorted_bams
 bcftools concat \
     --allow-overlaps \
-    --regions 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,X,Y \
     --output ${out_file} \
     --output-type z \
     --threads ${cores} \
