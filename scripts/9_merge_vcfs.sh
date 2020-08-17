5d516c7b90e87f94462eba5ba67ff6a3b4ee4026
NBMueller <nicoborgsmueller@web.de>
2020-08-17 12:09:00 +0200

    bugfix

--- scripts/9_merge_vcfs.sh
+++ scripts/9_merge_vcfs.sh
@@ -67,7 +67,7 @@ for reg in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 'X' 'Y'; do
     bcftools view \
         --regions $reg \
         --types snps \
-        --output ${out_dir}/all.${reg}.vcf.gz \
+        --output-file ${out_dir}/all.${reg}.vcf.gz \
         --output-type z \
     && bcftools index \
         --force \
