b0238f4c0be0ea322fd6c7257cce3b2a5d472988
NBMueller <nicoborgsmueller@web.de>
2020-10-06 14:04:51 +0200

    mutect filtering without contamination table added

--- scripts/08.2_mutect.sh
+++ scripts/08.2_mutect.sh
@@ -2,50 +2,35 @@
 
 module purge
 
-tumor_in=""
+sample_bams=""
 while [ "$1" != "" ]; do
     key=$1
     case ${key} in
         -m | --module)      shift
                             module load $1
                             ;;
-        -gAD | --gnomAD )   shift
-                            gnomAD=$1
+        -o | --out)         shift
+                            out_file=$1
                             ;;
-        -n | --normal )     shift
-                            normal_in="$1"
-                            ;;
-        *)                  tumor_in+="$1 "
+        *)                  sample_bams+="$1 " 
     esac
     shift
 done
 
-[[ -z "$gnomAD" ]] && { echo "Error: genome Aggregation Database not set"; exit 1; }
+[[ -z "$out_file" ]] && { echo "Error: Output file not set"; exit 1; }
 
-all_f1r2_input=`for chromosome in {1..22}; do
-        printf -- "-I Calls/${chromosome}.f1r2.mutect.tar.gz "; done`
-gatk LearnReadOrientationModel \
-    ${all_f1r2_input} \
-    --output Calls/read-orientation-model.tar.gz
+cores=$(nproc)
 
-## Normal bulk sample
-normal_name=$(echo $normal_in | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
-gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" GetPileupSummaries \
-    --input ${normal_in}  \
-    --variant ${gnomAD} \
-    --intervals ${gnomAD} \
-    --output Calls/getpileupsummaries.${normal_name}.table
+sorted_bams=$(echo "${sample_bams}"  | tr ' ' '\n' | sort -V | tr '\n' ' ') # | sed 's/$/.gz/'
 
-## Tumor bulk samples
-for tumor in $tumor_in; do
-    sample=$(echo $tumor | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
-    gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" GetPileupSummaries \
-        --input ${tumor}  \
-        --variant ${gnomAD} \
-        --intervals ${gnomAD} \
-        --output Calls/getpileupsummaries.${sample}.table \
-    && gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" CalculateContamination \
-        --input Calls/getpileupsummaries.${sample}.table \
-        --matched-normal Calls/getpileupsummaries.${normal_name}.table \
-        --output Calls/${sample}.contamination.table
-done
+bcftools concat \
+    --output ${out_file} \
+    --output-type z \
+    --threads ${cores} \
+    --no-version \
+    ${sorted_bams} \
+&& bcftools index \
+    --tbi \
+    --force \
+    --threads ${cores} \
+    ${out_file}
--- scripts/08.3_mutect.sh
+++ scripts/08.3_mutect.sh
@@ -2,35 +2,50 @@
 
 module purge
 
-sample_bams=""
+tumor_in=""
 while [ "$1" != "" ]; do
     key=$1
     case ${key} in
         -m | --module)      shift
                             module load $1
                             ;;
-        -o | --out)         shift
-                            out_file=$1
+        -gAD | --gnomAD )   shift
+                            gnomAD=$1
                             ;;
-        *)                  sample_bams+="$1 " 
+        -n | --normal )     shift
+                            normal_in="$1"
+                            ;;
+        *)                  tumor_in+="$1 "
     esac
     shift
 done
 
-[[ -z "$out_file" ]] && { echo "Error: Output file not set"; exit 1; }
+[[ -z "$gnomAD" ]] && { echo "Error: genome Aggregation Database not set"; exit 1; }
 
-cores=$(nproc)
+all_f1r2_input=`for chromosome in {1..22}; do
+        printf -- "-I Calls/${chromosome}.f1r2.mutect.tar.gz "; done`
+gatk LearnReadOrientationModel \
+    ${all_f1r2_input} \
+    --output Calls/read-orientation-model.tar.gz
 
-sorted_bams=$(echo "${sample_bams}"  | tr ' ' '\n' | sort -V | tr '\n' ' ') # | sed 's/$/.gz/'
+## Normal bulk sample
+normal_name=$(echo $normal_in | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" GetPileupSummaries \
+    --input ${normal_in}  \
+    --variant ${gnomAD} \
+    --intervals ${gnomAD} \
+    --output Calls/getpileupsummaries.${normal_name}.table
 
-bcftools concat \
-    --output ${out_file} \
-    --output-type z \
-    --threads ${cores} \
-    --no-version \
-    ${sorted_bams} \
-&& bcftools index \
-    --tbi \
-    --force \
-    --threads ${cores} \
-    ${out_file}
+## Tumor bulk samples
+for tumor in $tumor_in; do
+    sample=$(echo $tumor | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+    gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" GetPileupSummaries \
+        --input ${tumor}  \
+        --variant ${gnomAD} \
+        --intervals ${gnomAD} \
+        --output Calls/getpileupsummaries.${sample}.table \
+    && gatk --java-options "-Xmx35G -Djava.io.tmpdir=Calls/" CalculateContamination \
+        --input Calls/getpileupsummaries.${sample}.table \
+        --matched-normal Calls/getpileupsummaries.${normal_name}.table \
+        --output Calls/${sample}.contamination.table
+done
