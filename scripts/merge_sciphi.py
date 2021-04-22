897265c7ee694526600e0b9a414b3fcced107283
NBMueller <nicoborgsmueller@web.de>
2021-04-22 15:38:06 +0200

    bugfix

--- scripts/merge_sciphi.py
+++ scripts/merge_sciphi.py
@@ -117,7 +117,7 @@ def parse_args():
 
 if __name__ == '__main__':
     if 'snakemake' in globals():
-        if snakemake.full_run:
+        if snakemake.params.full_run:
             merge_standard(snakemake.input, snakemake.output)
         else:
             merge_readCounts(snakemake.input, snakemake.output)
