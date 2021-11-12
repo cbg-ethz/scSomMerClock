7e8c8851dcaf32406823dedc977bbe2898f40d8f
NBMueller <nicoborgsmueller@web.de>
2021-11-12 11:12:17 +0100

    plotting ADO adjusted

--- simulations/plotting/plot_ADOvar_pVal.py
+++ simulations/plotting/plot_ADOvar_pVal.py
@@ -24,7 +24,7 @@ def plot_ADO_dist(ADO, out_file):
 
 def generate_pval_plot(ADO_file, data_file, out_dir):
     ADO = pd.read_csv(ADO_file, sep='\t', index_col=0)
-    ADO.drop_duplicates(inplace=True)
+    ADO.drop_duplicates(keep='last', inplace=True)
 
     plot_ADO_dist(ADO, os.path.join(out_dir, 'ADO_dist.png'))
 
@@ -32,31 +32,33 @@ def generate_pval_plot(ADO_file, data_file, out_dir):
 
     methods = [i for i in data.columns if i.startswith('p-value')]
 
-    y = ADO.std(axis=1)
+    y_all = [('Mean', ADO.mean(axis=1)), ('StD', ADO.std(axis=1))]
     for method in methods:
         method_str = method.replace('p-value', '').strip('_')
         x = data[method]
+        for y_label, y in y_all:
+            plot_data = pd.concat([y, x] ,axis=1).dropna().values.T
 
-        plot_data = pd.concat([y, x] ,axis=1).dropna().values.T
+            fig, ax = plt.subplots(figsize=(16, 12))
+            plt.plot(plot_data[0], plot_data[1], marker='x', lw=0)
 
-        fig, ax = plt.subplots(figsize=(16, 12))
-        plt.plot(plot_data[0], plot_data[1], marker='x', lw=0)
+            ax.set_ylabel('p-Value', fontsize=LABEL_FONTSIZE)
+            ax.set_xlabel(f'ADO {y_label}', fontsize=LABEL_FONTSIZE)
+            ax.tick_params(axis='both', which='major', labelsize=TICK_FONTSIZE)
+            ax.tick_params(axis='both', which='minor', labelsize=TICK_FONTSIZE)
+            plt.title(f'Method: {method_str}')
 
-        ax.set_ylabel('p-Value', fontsize=LABEL_FONTSIZE)
-        ax.set_xlabel('ADO var', fontsize=LABEL_FONTSIZE)
-        ax.tick_params(axis='both', which='major', labelsize=TICK_FONTSIZE)
-        ax.tick_params(axis='both', which='minor', labelsize=TICK_FONTSIZE)
-        plt.title(f'Method: {method_str}')
-
-        out_file = os.path.join(out_dir, f'ADO_pVal_{method_str}.png')
-        fig.savefig(out_file, dpi=300)
-        plt.close()
+            out_file = os.path.join(out_dir, f'ADO_pVal_{method_str}_{y_label}.png')
+            fig.savefig(out_file, dpi=300)
+            plt.close()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--inDir', help='Input directory')
     parser.add_argument('-o', '--outDir', default='', help='Output directory')
+    parser.add_argument('-c', '--compress', action='store_true',
+        help='Compress output directory')
     args = parser.parse_args()
     return args
 
@@ -70,4 +72,8 @@ if __name__ == '__main__':
         os.makedirs(args.outDir, exist_ok=True)
 
     generate_pval_plot(ADO_file, data_file, args.outDir)
+    if args.compress:
+        import shutil
+        shutil.make_archive(args.outDir, 'zip', args.outDir)
+
 
