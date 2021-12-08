b04713b145281aea28229c5756c8f62a24e1dbf7
NBMueller <nicoborgsmueller@web.de>
2021-12-08 09:56:30 +0100

    bugfix in read LL

--- scripts/run_poisson_tests_all.py
+++ scripts/run_poisson_tests_all.py
@@ -24,6 +24,37 @@ def run_poisson_disp(vcf_files, exe, out_dir):
         raise RuntimeError('Error in Poisson Dispersion: {}'.format(stderr))
 
 
+def run_poisson_tree_cellphy(vcf_file, exe, paup_exe, out_dir):
+    path_strs = vcf_file.split(os.path.sep)
+    try:
+        clock_dir_no = path_strs.index('ClockTest')
+    except ValueError:
+        dataset = 'unknown'
+        subset = 'unknown'
+    else:
+        dataset = path_strs[clock_dir_no - 1]
+        subset = path_strs[clock_dir_no + 1]
+
+    out_file = os.path.join(out_dir, f'Poisson_tree_{dataset}_{subset}.tsv')
+    tree_file = vcf_file + '.raxml.bestTree'
+
+    cmmd = ' '.join(
+        ['python', exe, vcf_file, tree_file, '-o', out_file, '-e', paup_exe, '-b'])
+    print('\nShell command:\n{}\n'.format(cmmd))
+
+    poissonTree = subprocess.Popen(
+        cmmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = poissonTree.communicate()
+    poissonTree.wait()
+    stdout = str(stdout)
+
+    if stderr:
+        for i in stdout.split('\\n'):
+            print(i)
+        raise RuntimeError('Error in Poisson Tree: {}'.format(stderr))
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str,  help='vcf master file')
@@ -35,6 +66,8 @@ def parse_args():
     parser.add_argument('-ed', '--exe_disp', type=str,
         default='simulations/scripts/get_poisson_LRT.py',
         help='Poisson Dispersion exe.')
+    parser.add_argument('-p', '--exe_paup', type=str,
+        default='../paup4a168_ubuntu64', help='PAUP*  exe.')
     args = parser.parse_args()
     return args
 
@@ -47,11 +80,15 @@ if __name__ == '__main__':
 
     if not os.path.exists(args.out_dir):
         os.mkdir(args.out_dir)
-    run_poisson_disp(vcf_files, args.exe_disp, args.out_dir)
-    exit()
-    for vcf_file in vcf_files:
+    # <TODO> uncomment
+    # run_poisson_disp(vcf_files, args.exe_disp, args.out_dir)
 
-        import pdb; pdb.set_trace()
-        run_poisson_tree_cellphy(vcf_file, args.exe_tree)
-        run_poisson_tree_scite(vcf_file, args.exe_tree)
+    # <TODO> remove test case
+    test_vcf = '../data/Ni9/Ni8_cancer.vcf.gz'
+    run_poisson_tree_cellphy(test_vcf, args.exe_tree, args.exe_paup, args.out_dir)
+    # -----------------------
+
+    for vcf_file in vcf_files:
+        run_poisson_tree_cellphy(vcf_file, args.exe_tree, args.exe_paup, args.out_dir)
+        run_poisson_tree_scite(vcf_file, args.exe_tree, args.exe_paup, args.out_dir)
 
