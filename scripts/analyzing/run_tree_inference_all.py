472bca64d1e2d8560bca83e84bd12ba675f73652
NBMueller <nicoborgsmueller@web.de>
2022-01-20 16:29:32 +0100

    bugfix

--- scripts/analyzing/run_tree_inference_all.py
+++ scripts/analyzing/run_tree_inference_all.py
@@ -125,11 +125,11 @@ if __name__ == '__main__':
                         print(f'\tMissing vcf:\t\t{vcf_file}')
                         all_exist = False
                     if not os.path.exists(cellphy_out) and sub_dir != 'all' \
-                            or (subdir == 'all' and len(sub_dirs) > 1):
+                            or (sub_dir == 'all' and len(sub_dirs) > 1):
                         print(f'\tMissing cellphy tree:\t{cellphy_out}')
                         tree_exist = False
                     if not os.path.exists(scite_out) and sub_dir != 'all' \
-                            or (subdir == 'all' and len(sub_dirs) > 1):
+                            or (sub_dir == 'all' and len(sub_dirs) > 1):
                         print(f'\tMissing scite tree:\t{scite_out}')
                         tree_exist = False
                     continue
