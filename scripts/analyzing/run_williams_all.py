428aefb82290c442de4af3286b15ca000814857c
NBMueller <nicoborgsmueller@web.de>
2022-02-04 17:45:25 +0100

    bulk analysis added

---
+++ scripts/analyzing/run_williams_all.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import datetime
+import numpy as np
+import os
+import pandas as pd
+import shutil
+import subprocess
+import tarfile
+
+
+MODULE_STR = ''
+
+DEPTH = {
+    'Li55': {'SRR475153': 176.1 /2, 'SRR475160': 176.1 /2, 'SRR475185': 24.3},
+    'Ni8': {'SRR975206': 35.5, 'SRR975208': 49.1, 'SRR975210': 51.0,
+        'SRR975212': 65.9},
+    'W32': {'SRR1163508': 86.5, 'SRR1298936': 59.1},
+    'W55': {'SRR1153400': 71.6, 'SRR1153401': 18.1},
+    'Wu61': {'SRR3086496': 57.7, 'SRR3086497': 51.4, 'SRR3086498': 48.3},
+    'Wu63': {'CRC0827-Adenoma_Polyps': 58.8, 'CRC0827-Ca-1': 25,
+        'CRC0827-Ca-2': 29.9, 'CRC0827-Normal': 53.6},
+    'X25': {'SRR412866': 126.8, 'SRR412870': 37.3}
+}
+
+def run_bash(cmd_raw, bsub=True, module_str=MODULE_STR):
+    if bsub:
+        cmd = f"sbatch -t 30 -p amd-shared --qos amd-shared --mem 2G " \
+            f"--wrap '{module_str} {cmd_raw}'"
+    else:
+        cmd = f'{module_str} {cmd_raw}'
+
+    print(f'Running:\n{cmd}')
+    subp = subprocess.Popen(cmd,
+        shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = subp.communicate()
+    subp.wait()
+
+    if not bsub:
+        print(str(stdout), str(stderr))
+    print()
+
+
+def convert_vcf(vcf_file, args):
+    basename = os.path.splitext(os.path.basename(vcf_file))[0]
+    out_file = os.path.join(args.out_dir, basename)
+
+    if not args.replace:
+        existing = []
+        for file in os.listdir(args.out_dir):
+            if file.startswith(basename) and file.endswith('.VAF'):
+                existing.append(os.path.join(args.out_dir, file))
+        if len(existing) > 0:
+            print('Existing files:\n\t' + '\n\t'.join(existing))
+            return existing
+    cmd = f'python {args.exe_VAF} {vcf_file} -o {out_file} -b'
+    run_bash(cmd, False)
+
+    VAFs = []
+    for file in os.listdir(args.out_dir):
+        if file.startswith(basename) and file.endswith('.VAF'):
+            VAFs.append(os.path.join(args.out_dir, file))
+    return VAFs
+
+
+def run_williams(VAF_file, args):
+    basename = os.path.splitext(os.path.basename(VAF_file))[0]
+    out_file = os.path.join(args.out_dir, basename + '.mobster')
+
+    if not args.replace and os.path.exists(out_file):
+        print('Existing files: {out_file}')
+        return
+
+    dataset = basename.split('.')[0]
+    sample = basename.split('_')[-1]
+    try:
+        depth = DEPTH[dataset][sample]
+    except:
+        depth = 100
+    cellularity = 1
+
+    cmd = f'{args.exe_will} {VAF_file} {out_file} --depth {depth} ' \
+        f'--cellularity {cellularity}'
+    run_bash(cmd, args.local)
+
+
+def merge_datasets(args):
+    in_files = []
+    for file in os.listdir(args.input):
+        if file.endswith('.mobster'):
+            in_files.append(os.path.join(args.input, file))
+    out_file = os.path.join(args.out_dir, 'Summary_williams.tsv')
+
+    cmd = f'{args.exe_merge} {" ".join(in_files)} {out_file}'
+    run_bash(cmd, args.local)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str,
+        help='Input dir to files with pattern: <DATASET>.mutect2filtered.PASS.vcf')
+    parser.add_argument('-o', '--out_dir', type=str,
+        default='poisson_tests_all', help='Output file.')
+    parser.add_argument('-m', '--mode', type=str,
+        choices=['run', 'merge'], default='run',
+        help='Which task to do: run|merge')
+    parser.add_argument('-evaf', '--exe_VAF', type=str,
+        default='simulations/scripts/VCF_to_VAF.py',
+        help='VCF to VAF converter exe.')
+    parser.add_argument('-ewill', '--exe_will', type=str,
+        default='simulations/scripts/run_williams2016.R',
+        help='Williams 2016 R script exe.')
+    parser.add_argument('-emerge', '--exe_merge', type=str,
+        default='simulations/scripts/merge_bulk_summaries.py',
+        help='Merging script to combine all out files.')
+    parser.add_argument('-l', '--local', action='store_false',
+        help='Run locally instead of HPC.')
+    parser.add_argument('-r', '--replace', action='store_true',
+        help='Overwrite already existing files.')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if not os.path.exists(args.out_dir):
+        os.mkdir(args.out_dir)
+
+    if args.mode == 'run':
+        for in_file in os.listdir(args.input):
+            if not in_file.endswith('.mutect2filtered.PASS.vcf'):
+                continue
+            VAF_files = convert_vcf(os.path.join(args.input, in_file), args)
+            for VAF_file in VAF_files:
+                run_williams(VAF_file, args)
+    else:
+        merge_datasets(args)
+
+
+
+
--- simulations/scripts/VCF_to_VAF.py
+++ simulations/scripts/VCF_to_VAF.py
@@ -10,10 +10,11 @@ import numpy as np
 
 MUT = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
 MUT_REV = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
-MUT_VAR = {'A': np.array([1, 2, 3]), 'C': np.array([0, 2, 3]),
-    'G': np.array([0, 1, 3]), 'T': np.array([0, 1, 2])}
+# MUT_VAR = {'A': np.array([1, 2, 3]), 'C': np.array([0, 2, 3]),
+#     'G': np.array([0, 1, 3]), 'T': np.array([0, 1, 2])}
 
-def vcf_to_vaf(vcf_file, incl_re='', excl_re='healthycell', fmin=1e-6, fmax=1):
+def vcf_to_vaf_sc(vcf_file, incl_re='', excl_re='healthycell', fmin=1e-6, fmax=1):
+
     if vcf_file.endswith('gz'):
         file_stream = gzip.open(vcf_file, 'rb')
     else:
@@ -76,15 +77,12 @@ def vcf_to_vaf(vcf_file, incl_re='', excl_re='healthycell', fmin=1e-6, fmax=1):
 
                 reads += np.array(s_read, dtype=float)
 
-            var_all = MUT_VAR[line_cols[3]]
             DP = reads.sum()
-
             for alt_id, alt_count in enumerate(reads):
                 if alt_id == MUT[line_cols[3]]:
                     continue
                 alt = MUT_REV[alt_id]
                 vaf = alt_count / DP
-                # chr pos DP alt_count VAF
                 if vaf >= fmin and vaf < fmax:
                     out_str += f'\n{line_cols[0]}\t{line_cols[1]}\t' \
                         f'{line_cols[3]}\t{alt}\t{DP}\t{alt_count}\t{vaf:.6f}'
@@ -93,8 +91,76 @@ def vcf_to_vaf(vcf_file, incl_re='', excl_re='healthycell', fmin=1e-6, fmax=1):
     return np.array(vafs), out_str
 
 
+def vcf_to_vaf_bulk(vcf_file, fmin=1e-6, fmax=1, dmin=10):
+
+    if vcf_file.endswith('gz'):
+        file_stream = gzip.open(vcf_file, 'rb')
+    else:
+        file_stream = open(vcf_file, 'r')
+
+    with file_stream as f_in:
+        for line in f_in:
+            try:
+                line = line.decode()
+            except:
+                pass
+            # Skip VCF header lines
+            if line.startswith('#'):
+                # Safe column headers
+                if line.startswith('#CHROM'):
+                    sample_names = line.strip().split('\t')[9:]
+                    vafs = [[] for i in sample_names]
+                    out_str = ['chrom\tfrom\tref\talt\tDP\talt_count\tVAF'] \
+                        * len(sample_names)
+                    cnt = np.zeros(len(sample_names))
+                continue
+            elif line.strip() == '':
+                continue
+
+            line_cols = line.strip().split('\t')
+            elem_ids = line_cols[8].split(':')
+
+            # Skip indels
+            if len(line_cols[3]) > 1 or len(line_cols[4]) > 1:
+                continue
+
+            for s_i, s_rec in enumerate(line_cols[9:]):
+                s_rec_elms = s_rec.split(':')
+                # Skip wt
+                if s_rec_elms[0] == '0/0' or s_rec_elms[0] == '0|0':
+                    continue
+                cnt[s_i] += 1
+                try:
+                    alt_count = s_rec_elms[ad_id].split(',')[1]
+                    DP = int(s_rec_elms[depth_id])
+                    vaf = float(s_rec_elms[vaf_id])
+                except NameError:
+                    ad_id = elem_ids.index('AD')
+                    depth_id = elem_ids.index('DP')
+                    vaf_id = elem_ids.index('AF')
+
+                    alt_count = s_rec_elms[ad_id].split(',')[1]
+                    DP = int(s_rec_elms[depth_id])
+                    vaf = float(s_rec_elms[vaf_id])
+
+                if vaf >= fmin and vaf < fmax and DP >= dmin:
+                    out_str[s_i] += f'\n{line_cols[0]}\t{line_cols[1]}\t' \
+                        f'{line_cols[3]}\t{line_cols[4]}\t{DP}\t{alt_count}\t' \
+                        f'{vaf:.6f}'
+                    vafs[s_i].append(vaf)
+
+    excl = [i for i, j in enumerate(vafs) if len(j) == 0]
+    vafs = [j for i, j in enumerate(vafs) if i not in excl]
+    out_str = [j for i, j in enumerate(out_str) if i not in excl]
+
+    return vafs, zip(sample_names, out_str)
+
+
 def show_VAF_dist(arr):
     import matplotlib.pyplot as plt
+
+    if isinstance(arr, list):
+        arr = np.array(arr)
 
     fig, ax = plt.subplots(figsize=(16, 12))
     ax.hist(arr, bins=100, range= (0, 1))
@@ -109,7 +175,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('vcf', type=str, help='SNP file in VCF format.')
     parser.add_argument('-o', '--output', type=str, default='',
-        help='Output file. Default: <VCF>.VAF')
+        help='Output file/dir. Default: <VCF>.VAF')
     parser.add_argument('-min', '--fmin', type=float, default=1e-6,
         help='Minimum VAF value: <= are excluded. Default: 0.001')
     parser.add_argument('-max', '--fmax', type=float, default=1.0,
@@ -118,6 +184,10 @@ def parse_args():
         help='Regex pattern for samples to include.')
     parser.add_argument('-excl', '--exclude', type=str, default='',
         help='Regex pattern for samples to exclude.')
+    parser.add_argument('-b', '--bulk', action='store_true',
+        help='If set, assumes mutect2 VCF output.')
+    parser.add_argument('-dmin', '--depthmin', type=int, default=10,
+        help='Min. depth for bulk/biological data.')
     args = parser.parse_args()
     return args
 
@@ -125,19 +195,29 @@ def parse_args():
 if __name__ == '__main__':
     if 'snakemake' in globals():
         vafs, out_str = vcf_to_vaf(snakemake.input.vcf)
-        out_file = snakemake.output.vaf
+        with open(snakemake.output.vaf, 'w') as f:
+            f.write(out_str)
+        os.remove(snakemake.input.vcf)
     else:
         args = parse_args()
-        vafs, out_str = vcf_to_vaf(args.vcf, args.include, args.exclude, args.fmin, args.fmax)
-
         if args.output:
             out_file = args.output
         else:
-            out_file = args.vcf + '.VAF'
+            out_file = args.vcf
 
-    with open(out_file, 'w') as f:
-        f.write(out_str)
+        if args.bulk:
+            vafs, out_data = vcf_to_vaf_bulk(args.vcf, args.fmin, args.fmax,
+                args.depthmin)
 
-    if 'snakemake' in globals():
-        os.remove(snakemake.input.vcf)
+            for sample_name, out_str in out_data:
+                out_file_s = f'{out_file}_{sample_name}.VAF'
+                with open(out_file_s, 'w') as f:
+                    f.write(out_str)
+        else:
+            vafs, out_str = vcf_to_vaf_sc(args.vcf, args.include, args.exclude,
+                args.fmin, args.fmax)
+
+            with open(out_file, 'w') as f:
+                f.write(out_str)
+
     # show_VAF_dist(vafs)
